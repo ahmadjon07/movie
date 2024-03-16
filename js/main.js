d22ed4f1c127f7358fc8e20d665a6e8a397646a8
ahmadjon07 <ahmadzonzulunov1@gmail.com>
2024-03-16 17:10:55 +0300

    movie3

--- js/main.js
+++ js/main.js
@@ -1,23 +1,6 @@
 let elMovList = document.querySelector('.movie__list')
 let elSel__category = document.querySelector('.sel__category')
-let  partMovie = movies.splice(1,900)
-partMovie.forEach((item)=>{
-    let newLi = document.createElement('li')
-    newLi.innerHTML  = `
-    <div class="card" style="width: 18rem;">
-  <img src="	https://i.ytimg.com/vi/${item.ytid}/hq720.jpg?" class="card-img-top" alt="...">
-  <div class="card-body">
-    <h5 class="card-title">${item.Title}</h5>
-    <p class="card-text">${item.fulltitle}</p>
-    <p class="card-text">${item.movie_year}</p>
-    <p class="card-text">${item.imdb_rating}</p>
-    <a href="https://www.youtube.com/watch?v=${item.ytid}" target="_blank" class="btn btn-primary">Go somewhere</a>
-  </div>
-</div>
-    `
-    console.log(item.Title);
-    elMovList.appendChild(newLi)
-});
+let  partMovie = movies.splice(1,100)
 
 fnRender(partMovie)
 function fnRender(data) {
